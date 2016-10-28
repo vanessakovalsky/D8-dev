27ae77b02f0268e2a207601f810c14d8d6c69ff0
vanessakovalsky <vanessa.kovalsky@free.fr>
2016-10-28 08:19:40 +0000

    Ajout Field formatter

---
+++ demo_formation/src/Plugin/Field/FieldFormatter/RandomFieldFormatter.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Random\Plugin\Field\FieldFormatter\RandomDefaultFormatter.
+ */
+
+namespace Drupal\demo_formation\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'Random_default' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "Random_default",
+ *   label = @Translation("Random text"),
+ *   field_types = {
+ *     "string"
+ *   }
+ * )
+ */
+class RandomFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = array();
+    $settings = $this->getSettings();
+
+    $summary[] = t('Displays the random string.');
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = array();
+
+    foreach ($items as $delta => $item) {
+      // Render each element as markup.
+      $element[$delta] = array(
+        '#type' => 'markup',
+        '#markup' => $item->value,
+      );
+    }
+
+    return $element;
+  }
+}
