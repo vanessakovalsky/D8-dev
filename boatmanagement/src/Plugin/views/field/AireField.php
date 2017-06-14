6c98693fec0e4100d744d9e825c9f742035181ab
Vanessa David <vanessa.david3@docapost.fr>
2017-06-14 16:19:52 +0200

    Add example for practical exercise

---
+++ boatmanagement/src/Plugin/views/field/AireField.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\boatmanagement\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Random;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("aire_field")
+ */
+class AireField extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['hide_alter_empty'] = ['default' => FALSE];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    // Return a random text, here you can include your custom logic.
+    // Include any namespace required to call the method required to generate
+    // the desired output.
+    $longueur = $values->_entity->field_longueur->value;
+    $largeur = $values->_entity->field_largeur->value;
+    $aire = $longueur * $largeur;
+    return $aire;
+  }
+
+}
