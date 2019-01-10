05055453e051abdf44c6d42fc6d745a8b07db690
vanessakovalsky <vanessa.kovalsky@free.fr>
2019-01-10 09:23:30 +0100

    Ajout block calcul aire

---
+++ boatmanagement/src/Plugin/Block/CalculAireBlock.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\boatmanagement\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides a 'CalculAireBlock' block.
+ *
+ * @Block(
+ *  id = "calcul_aire_block",
+ *  admin_label = @Translation("Calcul aire block"),
+ * )
+ */
+class CalculAireBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+  /**
+   * Constructs a new CalculAireBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager,
+	RequestStack $request_stack
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->requestStack = $request_stack;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('request_stack')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+      $node = $this->requestStack->getCurrentRequest()->attributes->get('node');
+      $largeur = $node->get('field_largeur')->getValue()[0]['value'];
+      $longueur = $node->get('field_longueur')->getValue()[0]['value'];
+      $aire = $longueur * $largeur;
+      $build['calcul_aire_block']['#markup'] = 'L\'aire du bateau est égale à :'.$aire;
+    return $build;
+  }
+
+  public function getCacheTags() {
+    //With this when your node change your block will rebuild
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      //if there is node add its cachetag
+      return Cache::mergeTags(parent::getCacheTags(), array('node:' . $node->id()));
+    } else {
+      //Return default tags instead.
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    //if you depends on \Drupal::routeMatch()
+    //you must set context of this block with 'route' context tag.
+    //Every new route this block will rebuild
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
+  }
+
+}
